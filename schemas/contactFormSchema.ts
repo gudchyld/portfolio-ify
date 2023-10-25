8fd6b704d08dee189f836d555978037394206c1f
Elendu Ifeanyi <goodchild66@gmail.com>
2023-10-25 12:57:43 +0100

    radical folder change

--- schemas/contactFormSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-export const contactFormSchema = z.object({
-  username: z.string().min(3).max(20),
-  email: z.string().email(),
-  message: z.string().min(10),
-});
-
-type ContactFormValues = z.infer<typeof contactFormSchema>;
